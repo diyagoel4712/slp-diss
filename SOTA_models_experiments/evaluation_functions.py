ef32742375c597367a82de4508088b269ed00440
Diya Goel <s2247837@ed.ac.uk>
2026-06-11 19:14:51 +0100

    synthesise-from-existing-models

---
+++ SOTA_models_experiments/evaluation_functions.py
@@ -0,0 +1,29 @@
+# define evaluation metrics -- to be called in an .ipynb file
+
+# 1. UTMOS
+def utmos():
+    pass
+
+# 2. F0 RMSE
+def f0_rmse():
+    pass
+
+# 3. MCD
+def mcd():
+    pass
+
+# 4. WER
+def wer():
+    pass
+
+# 5. AccentID acc.
+def aid_acc():
+    pass
+
+# 6. CS for accent embeddings
+def cs_accent():
+    pass
+
+# 7. PPG-KL
+def ppg_kl():
+    pass
